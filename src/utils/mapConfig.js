30c443ea562685ab5a3c8720b3be117db3199fa8
weltme <tjlwelt@foxmail.com>
2023-07-19 10:54:21 +0800

    更改osskey

--- src/utils/mapConfig.js
+++ src/utils/mapConfig.js
@@ -1,4 +1,4 @@
 export default {
-    JSAPIKey: '你的高德地图JSAPI的Key',
-    WebAPIKey: '你的高德地图WebAPI的Key'
+  JSAPIKey: '09e6391bbef7070a5fc4c6db5e306338',
+  WebAPIKey: '647b17e5508e7ae00b122df635fa4e25',
 }
