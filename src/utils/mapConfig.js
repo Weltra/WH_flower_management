44f1840782b9e4f1f87a0126dfca473016d605f9
weltme <tjlwelt@foxmail.com>
2023-07-19 11:36:05 +0800

    update .gitignore

--- src/utils/mapConfig.js
+++
@@ -1,4 +0,0 @@
-export default {
-  JSAPIKey: '你的高德地图JSAPI的Key',
-  WebAPIKey: '你的高德地图WebAPI的Key',
-}
--- src/utils/ossConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    options_OSS: {
-        region: '你的阿里云Oss所在的区域',
-        accessKeyId: '你的阿里云Oss的Key',
-        accessKeySecret: '你的阿里云Oss的密码',
-        bucket: '你的阿里云Oss的bucket名称'
-    },
-}
